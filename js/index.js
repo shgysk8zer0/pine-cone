e820c16d711741aeba9d308af85445690cdc5a69
Chris Zuber <shgysk8zer0@gmail.com>
2020-03-10 14:26:50 -0700

    Wait for DOM available before modifying class of `<body>`

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
 import 'https://cdn.kernvalley.us/js/std-js/deprefixer.js';
 import 'https://cdn.kernvalley.us/js/std-js/shims.js';
 import 'https://cdn.kernvalley.us/components/current-year.js';
-import { registerServiceWorker } from 'https://cdn.kernvalley.us/js/std-js/functions.js';
+import { registerServiceWorker, ready } from 'https://cdn.kernvalley.us/js/std-js/functions.js';
 
 if (document.documentElement.dataset.hasOwnProperty('serviceWorker')) {
 	registerServiceWorker(document.documentElement.dataset.serviceWorker).catch(console.error);
 }
 
 document.documentElement.classList.replace('no-js', 'js');
-document.body.classList.toggle('no-dialog', document.createElement('dialog') instanceof HTMLUnknownElement);
-document.body.classList.toggle('no-details', document.createElement('details') instanceof HTMLUnknownElement);
+
+ready().then(() => {
+	document.body.classList.toggle('no-dialog', document.createElement('dialog') instanceof HTMLUnknownElement);
+	document.body.classList.toggle('no-details', document.createElement('details') instanceof HTMLUnknownElement);
+});
