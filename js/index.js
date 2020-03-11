90390879fb016cd7b08c563b24112ca0cbb258c6
Chris Zuber <shgysk8zer0@gmail.com>
2020-03-11 16:53:13 -0700

    Add disclaimer & update misc. styles
    **Accessibility disclaimer is currently only lorem ipsum.**
    
    - Fix bad script in dev
    - Customize footer
    - Add hidden header content from current site
    - Add logo
    - Make site functional with no JS / JS errors
    - Toggle `<dialog>`s by hash change in URL
    - CSS fallback for `<dialog>`s
    - Remove & ignore generated icons

--- js/index.js
+++ js/index.js
@@ -1,15 +1,37 @@
 import 'https://cdn.kernvalley.us/js/std-js/deprefixer.js';
 import 'https://cdn.kernvalley.us/js/std-js/shims.js';
-import 'https://cdn.kernvalley.us/components/current-year.js';
 import { registerServiceWorker, ready } from 'https://cdn.kernvalley.us/js/std-js/functions.js';
+
+document.documentElement.classList.replace('no-js', 'js');
 
 if (document.documentElement.dataset.hasOwnProperty('serviceWorker')) {
 	registerServiceWorker(document.documentElement.dataset.serviceWorker).catch(console.error);
 }
 
-document.documentElement.classList.replace('no-js', 'js');
-
 ready().then(() => {
 	document.body.classList.toggle('no-dialog', document.createElement('dialog') instanceof HTMLUnknownElement);
 	document.body.classList.toggle('no-details', document.createElement('details') instanceof HTMLUnknownElement);
+	hashChangeHandler({newURL: location.href});
 });
+
+function hashChangeHandler({newURL, oldURL = null})  {
+	const [from, to] = [
+		typeof oldURL === 'string' ? new URL(oldURL).hash.substr(1) : null,
+		new URL(newURL).hash.substr(1)
+	];
+
+	const target = document.getElementById(to);
+	const leaving = document.getElementById(from);
+
+	if (target instanceof HTMLElement && target.tagName === 'DIALOG') {
+		target.showModal();
+	} else if (target instanceof HTMLElement && target.tagName === 'TOAST-MESSAGE') {
+		customElements.whenDefined('toast-message').then(() => target.show());
+	}
+
+	if (leaving instanceof HTMLElement && ['DIALOG', 'TOAST-MESSAGE'].includes(leaving.tagName)) {
+		leaving.close();
+	}
+}
+
+window.addEventListener('hashchange', ({newURL, oldURL}) => hashChangeHandler({newURL, oldURL}), {passive: true});
