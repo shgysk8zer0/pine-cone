8e3efa210e1fa94eeedb9d934b3495009fbf8ba9
Chris Zuber <shgysk8zer0@gmail.com>
2020-03-12 15:57:37 -0700

    Prevent default on accessibility dialog close click
    Instead, use `history.back()` to avoid jumping around page.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 import 'https://cdn.kernvalley.us/js/std-js/deprefixer.js';
 import 'https://cdn.kernvalley.us/js/std-js/shims.js';
-import { registerServiceWorker, ready } from 'https://cdn.kernvalley.us/js/std-js/functions.js';
-import { hashChange } from './handlers.js';
+import { $, registerServiceWorker, ready } from 'https://cdn.kernvalley.us/js/std-js/functions.js';
+import * as handlers from './handlers.js';
 document.documentElement.classList.replace('no-js', 'js');
 
 if (document.documentElement.dataset.hasOwnProperty('serviceWorker')) {
@@ -11,8 +11,15 @@ if (document.documentElement.dataset.hasOwnProperty('serviceWorker')) {
 ready().then(() => {
 	document.body.classList.toggle('no-dialog', document.createElement('dialog') instanceof HTMLUnknownElement);
 	document.body.classList.toggle('no-details', document.createElement('details') instanceof HTMLUnknownElement);
-	hashChange({newURL: location.href});
+	handlers.hashChange({newURL: location.href});
+	$('[data-action]').click(event => {
+		const action = event.target.closest('[data-action]').dataset.action;
+
+		if (action in handlers) {
+			handlers[action](event);
+		}
+	});
 });
 
 
-window.addEventListener('hashchange', ({newURL, oldURL}) => hashChange({newURL, oldURL}), {passive: true});
+window.addEventListener('hashchange', ({newURL, oldURL}) => handlers.hashChange({newURL, oldURL}), {passive: true});
