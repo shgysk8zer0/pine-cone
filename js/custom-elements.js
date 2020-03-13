4f84c57031b86725870113ad8748314284b097b5
Chris Zuber <shgysk8zer0@gmail.com>
2020-03-13 11:08:03 -0700

    Update scripts to work async
    Move polyfill.io & custom elements shim into scripts that require them in order to allow scripts to work async

---
+++ js/custom-elements.js
@@ -0,0 +1,4 @@
+import 'https://unpkg.com/@webcomponents/custom-elements@1.3.2/custom-elements.min.js';
+import 'https://cdn.kernvalley.us/components/share-button.js';
+import 'https://cdn.kernvalley.us/components/leaflet/map.js';
+import 'https://cdn.kernvalley.us/components/leaflet/marker.js';
